dca102580c2bd6536f2f1a1d99d48366ebca016c
tommycs127 <tommyyung1@hotmail.com>
2018-12-06 13:31:01 +0800

    Second commit

---
+++ src/com/pccu/ImageCopy/ImageCopy.java
@@ -0,0 +1,49 @@
+package com.pccu.ImageCopy;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ImageCopy {
+	
+	public static void Copy(String i, String o) {
+		if (i == o) {
+			System.out.println();
+			return;
+		}
+		
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		
+		try {
+			fis = new FileInputStream("src/images/"+i);
+			fos = new FileOutputStream("src/images/"+o);
+			int flag = 0;
+			while((flag = fis.read()) != -1) {
+				fos.write(flag);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null) fis.close();
+				if (fos != null) fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Copy completed.");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Copy("pedobear.gif",
+			 "pedobear_2.gif"
+		);
+		
+	}
+
+}
